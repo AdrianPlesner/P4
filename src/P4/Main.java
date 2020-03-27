ebacaf4c297caeee64858e56b00ac9bb007933eb
Adrian Plesner <adrian.plesner@gmail.com>
2020-03-27 14:51:30 +0100

    Grammar updated. A parser

--- src/P4/Main.java
+++ src/P4/Main.java
@@ -1,7 +1,40 @@
 package P4;
+
+import P4.Sable.parser.*;
+import P4.Sable.lexer.*;
+import P4.Sable.node.*;
+
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main( String[] args){
         System.out.println("Compiling..");
+        long start_time, stop_time; //times compilation
+
+        if (args.length < 1) {
+            System.out.println("Usage:");
+            System.out.println(" java P4.Main <filename>");
+        }
+
+        try {
+            start_time = System.currentTimeMillis();
+
+               // create lexer
+            Lexer lexer = new Lexer (new PushbackReader(new BufferedReader(new FileReader(args[0])), 1024));
+
+               // parser program
+            Parser parser = new Parser(lexer);
+
+            Start ast = parser.parse();
+
+            stop_time = System.currentTimeMillis();
+
+            System.out.println("Compilation took " + (stop_time-start_time)/1000 + " seconds" );
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
 
